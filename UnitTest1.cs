374f84bd7d5897febaf4cce998e0528db793cb2e
SunRiseGG <SunRiseGC4@gmail.com>
2020-11-22 15:50:26 +0300

    fix

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -7,6 +7,13 @@ namespace Lab2 {
     public class UnitTest1 {
 
         [TestMethod]
+        public void TestConstructorLength1() {
+            ulong tmp = 42069;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp);
+            Assert.IsTrue(test1.GetFlag());
+        }
+
+        [TestMethod]
         public void TestFlagTrue() {
             ulong tmp = 42069;
             MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
@@ -28,19 +35,79 @@ namespace Lab2 {
         }
 
         [TestMethod]
-        public void TestMaxValue() {
+        public void TestMaxValue1() {
             ulong tmp = UInt64.MaxValue;
             MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
             Assert.IsTrue(test1.GetFlag());
         }
 
         [TestMethod]
-        public void TestReset() {
+        public void TestMaxValue2() {
+            ulong tmp1 = 17179868704;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp1, true);
+            Assert.IsTrue(test1.GetFlag());
+        }
+
+        [TestMethod]
+        public void TestFlagLength() {
+            ulong tmp = 42069;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
+            Assert.Equals(tmp, test1.ToString().Length);
+        }
+
+        [TestMethod]
+        public void TestFlagToString() {
+            ulong tmp = 3;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
+            MultipleBinaryFlag test2 = new MultipleBinaryFlag(tmp, false);
+            Assert.Equals(test1.ToString(), "TTT");
+            Assert.Equals(test2.ToString(), "FFF");
+        }
+
+        [TestMethod]
+        public void TestObjectReference1() {
+            ulong tmp = 3;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
+            MultipleBinaryFlag test2 = test1;
+            Assert.Equals(test1, test2);
+        }
+
+        [TestMethod]
+        public void TestObjectReference2() {
+            ulong tmp = 3;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
+            MultipleBinaryFlag test2 = new MultipleBinaryFlag(tmp, true);
+            Assert.AreNotEqual(test1, test2);
+        }
+
+        [TestMethod]
+        public void TestReset1() {
             ulong tmp1 = 42069;
             ulong position = 420;
             MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp1, true);
             test1.ResetFlag(position);
             Assert.IsFalse(test1.GetFlag());
+        }
+
+        [TestMethod]
+        public void TestReset2() {
+            ulong tmp1 = 9;
+            ulong position = 6;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp1, true);
+            test1.ResetFlag(position);
+            Assert.Equals(test1.ToString(), "TTTTTTFTTT");
+        }
+
+        [TestMethod]
+        public void TestMultipleReset() {
+            ulong tmp = 2;
+            ulong position1 = 0;
+            ulong position2 = 1;
+            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, true);
+            test1.ResetFlag(position1);
+            test1.ResetFlag(position2);
+            Assert.IsTrue(test1.GetFlag());
+            Assert.Equals(test1.ToString(), "TT");
         }
 
         [TestMethod]
@@ -78,17 +145,6 @@ namespace Lab2 {
             ulong position = 0;
             MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, false);
             test1.SetFlag(position);
-            Assert.IsFalse(test1.GetFlag());
-        }
-
-        [TestMethod]
-        public void TestMultipleReset() {
-            ulong tmp = 2;
-            ulong position1 = 0;
-            ulong position2 = 1;
-            MultipleBinaryFlag test1 = new MultipleBinaryFlag(tmp, false);
-            test1.ResetFlag(position1);
-            test1.ResetFlag(position2);
             Assert.IsFalse(test1.GetFlag());
         }
 
